c2830b7357a2bf970cd0e5c320cb55b128c46757
shinu shaji <shinushaji10@gmail.com>
2021-02-05 14:49:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const server = http.createServer((req,res)=>{
 					var intendData = jsonData["intent"]["query"];
 					//console.log()
 					console.log(intendData);
-					http.get("http://184.144.70.115:8082/");
+					http.get("http://184.144.70.115:8082/"+intendData);
 					});
 		
 		if(req.method=="GET"){
