0544538953a381d0485c7bd659a892f39db2a645
shinushaji10@gmail.com <shinushaji10@gmail.com>
2021-02-04 20:24:29 -0500

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
-const  http = require('http');
-const  port = process.env.PORT || 1234;
+const http = require('http');
+const port = process.env.PORT||1234;
+const path = require("path");
+const fs =   require("fs");
+
 
 const server = http.createServer((req,res)=>{
-	console.log(req.headers);
-	res.statusCode=200;
+		var jsonString=null;
+		req.on("data",(data)=>{jsonString+=data;});
+		req.on("end",()=>{console.log(JSON.parse(jsonString));});
+		
+		
+		if(req.method=="GET"){
+			var fileUrl;
+			if(req.url=="/")fileUrl="/index.html";
+			else fileUrl = req.url;
+			var filePath = path.resolve("."+fileUrl);
+			const fileExt = path.extname(filePath);
+			if(fileExt ==".html"){
+				fs.exists(filePath,(exists)=>{
+				if(!exists){
+					res.statusCode="404";
+					res.setHeader("Content-Type","text/html");
+					res.end("<html><body><h1>"+fileUrl+"not found</h1></body></html>");
+					return;
+					}
+				res.statusCode="200";
+				res.setHeader("Content-Type","text/html");
+				fs.createReadStream(filePath).pipe(res);
+				return;
+				});
+			}
+			else{
+				res.statusCode="404";
+				res.setHeader("Content-Type","text/html");
+				res.end("<html><body><h1>"+fileUrl+"not html</h1></body></html>");
+				return;
+
+				}
+		}
+		else{
+			res.statusCode="404";
+			res.setHeader("Content-Type","text/html");
+			res.end("<html><body><h1>"+req.method+"not found</h1></body></html>");
+			return;
+		}		
+				
 	}
 );
 
-server.listen(port,()=>{
-		console.log("server running");
-	}
-	);
+server.listen(port);
+
+
